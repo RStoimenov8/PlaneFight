ea51dc5a4f03e45f3f9fe50c87755286a6aa2b27
Rosen Stoimenov <streamer_bg@abv.bg>
2025-02-16 02:55:12 +0200

    upload

---
+++ PlaneFight/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PlaneFight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Bullet = new System.Windows.Forms.PictureBox();
+            this.Ememy1 = new System.Windows.Forms.PictureBox();
+            this.Ememy2 = new System.Windows.Forms.PictureBox();
+            this.Ememy3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ememy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ememy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ememy3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Image = global::PlaneFight.Properties.Resources.player;
+            this.Player.Location = new System.Drawing.Point(260, 530);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(112, 101);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // Bullet
+            // 
+            this.Bullet.Image = global::PlaneFight.Properties.Resources.bullet;
+            this.Bullet.Location = new System.Drawing.Point(311, 378);
+            this.Bullet.Name = "Bullet";
+            this.Bullet.Size = new System.Drawing.Size(10, 30);
+            this.Bullet.TabIndex = 1;
+            this.Bullet.TabStop = false;
+            // 
+            // Ememy1
+            // 
+            this.Ememy1.Image = global::PlaneFight.Properties.Resources.enemy;
+            this.Ememy1.Location = new System.Drawing.Point(61, 48);
+            this.Ememy1.Name = "Ememy1";
+            this.Ememy1.Size = new System.Drawing.Size(103, 88);
+            this.Ememy1.TabIndex = 2;
+            this.Ememy1.TabStop = false;
+            // 
+            // Ememy2
+            // 
+            this.Ememy2.Image = global::PlaneFight.Properties.Resources.enemy;
+            this.Ememy2.Location = new System.Drawing.Point(276, 48);
+            this.Ememy2.Name = "Ememy2";
+            this.Ememy2.Size = new System.Drawing.Size(103, 88);
+            this.Ememy2.TabIndex = 3;
+            this.Ememy2.TabStop = false;
+            // 
+            // Ememy3
+            // 
+            this.Ememy3.Image = global::PlaneFight.Properties.Resources.enemy;
+            this.Ememy3.Location = new System.Drawing.Point(459, 48);
+            this.Ememy3.Name = "Ememy3";
+            this.Ememy3.Size = new System.Drawing.Size(103, 88);
+            this.Ememy3.TabIndex = 4;
+            this.Ememy3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(258, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 108);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(103, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(436, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Game Pused! Press Space to Resume/Start!";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 667);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.Bullet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ememy1);
+            this.Controls.Add(this.Ememy2);
+            this.Controls.Add(this.Ememy3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.onKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.onKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ememy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ememy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ememy3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Bullet;
+        private System.Windows.Forms.PictureBox Ememy1;
+        private System.Windows.Forms.PictureBox Ememy2;
+        private System.Windows.Forms.PictureBox Ememy3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PlaneFight/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PlaneFight
+{
+    public partial class Form1 : Form
+    {
+        //playerMoves
+        int moveLeft = 0;
+        int EnemyMove = 5;
+        Random r = new Random();
+        int bulletSpd = -8;
+        bool shooting = false;
+        int score = 0;
+        public Form1()
+        {
+           
+            InitializeComponent();
+            Ememy1.Top = -500;
+            Ememy1.Top = -900;
+            Ememy1.Top = -1300;
+            Bullet.Top = -100;
+            Bullet.Left = -100;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkTurquoise;
+            this.Text = "Plane Game";
+            Thread.Sleep(2000);
+            label2.Visible = true;
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Down)
+            {
+                timer1.Stop();
+                label2.Visible = true;
+            }
+            if (e.KeyCode==Keys.PageUp)
+            {
+                score += 10;
+            }
+            if (e.KeyCode==Keys.Space)
+            {
+                timer1.Enabled = true;
+                label2.Visible = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                if (Player.Location.X<0)
+                {
+                    moveLeft = 0;
+                }
+                else
+                {
+                    moveLeft = -5;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (Player.Location.X >512)
+                {
+                    moveLeft = 0;
+                }
+                else
+                {
+                    moveLeft = 5;
+                }
+                
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (shooting == false)
+                {
+                    shooting = true;
+                    bulletSpd = -8;
+                    Bullet.Left = Player.Left + 50;
+                    Bullet.Top = Player.Top;
+                    shooting = true;
+                }
+            }
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = 0;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveLeft = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            enemyHit();
+            Player.Left += moveLeft;
+            Bullet.Top += bulletSpd;
+            Ememy1.Top += EnemyMove;
+            Ememy2.Top += EnemyMove;
+            Ememy3.Top += EnemyMove;
+            label1.Text = "" + score;
+            if (Ememy1.Top == 660 || Ememy2.Top == 660 || Ememy3.Top == 660)
+            {
+                GameOver();
+            }
+            if (shooting && Bullet.Top<0)
+            {
+                bulletSpd = 0;
+                Bullet.Top = -100;
+                Bullet.Left = -100;
+                
+            }
+            if (Bullet.Top>ClientSize.Height || Bullet.Top<0)
+            {
+                shooting = false;
+            }
+            
+                
+        }
+        private void enemyHit()
+        {
+            if (Bullet.Bounds.IntersectsWith(Ememy1.Bounds))
+            {
+                score++;
+                Ememy1.Top = -510;
+                int ranP = r.Next(1, 300);
+                Ememy1.Left = ranP;
+                shooting = false;
+                bulletSpd = 0;
+                Bullet.Top = -100;
+                Bullet.Left = -100;
+                shooting = false;
+
+            }
+            if (Bullet.Bounds.IntersectsWith(Ememy2.Bounds))
+            {
+                score++;
+                Ememy1.Top = -510;
+                int ranP = r.Next(1, 300);
+                Ememy1.Left = ranP;
+                shooting = false;
+                bulletSpd = 0;
+                Bullet.Top = -100;
+                Bullet.Left = -100;
+
+            }
+            if (Bullet.Bounds.IntersectsWith(Ememy3.Bounds))
+            {
+                score++;
+                Ememy1.Top = -500;
+                int ranP = r.Next(1, 300);
+                Ememy1.Left = ranP;
+                shooting = false;
+                bulletSpd = 0;
+                Bullet.Top = -100;
+                Bullet.Left = -100;
+
+            }
+        }
+        private void GameOver()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("Score: " + score + "\r\nContinue?" );
+            score = 0;
+            Ememy1.Top = -500;
+            Ememy2.Top = -900;
+            Ememy3.Top = -1300;
+            Bullet.Top = -100;
+            Bullet.Left = -100;
+            timer1.Enabled = true;
+            Application.Restart();
+        }
+        private void Pause()
+        {
+
+        }
+    }
+}
